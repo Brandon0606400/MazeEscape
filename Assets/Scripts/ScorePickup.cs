fbf20bca61a3543ecc71cc722303f97d83ee1c57
Brandon0606400 <43067373+Brandon0606400@users.noreply.github.com>
2018-10-05 14:50:27 +0100

    Added Grid and Camera Follow

---
+++ Assets/Scripts/ScorePickup.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScorePickup : MonoBehaviour {
+
+    public Score scoreTracker;
+    public int scoreValue = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Only do stuff if it's the player we've hit
+        if (collision.GetComponent<Player>())
+        {
+            // updating the score based on how much this item is worth
+            scoreTracker.ChangeValue(scoreValue);
+            
+            // remove the item when it is used
+            Destroy(gameObject);
+        }
+    }
+
+
+
+}
